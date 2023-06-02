173dbed85aa921680bfd4e2d49ce2d4ae378a7dc
Jesus Glez <jesusgleztr94@gmail.com>
2023-06-01 20:58:03 -0600

    Hotfix: Formated

--- resources/js/react/components/ScrollToTop.jsx
+++ resources/js/react/components/ScrollToTop.jsx
@@ -1,21 +1,20 @@
-import React, { useEffect } from "react";
-import { useLocation } from "react-router-dom";
+import React, { useEffect } from 'react'
+import { useLocation } from 'react-router-dom'
 
 const ScrollToTop = () => {
-    const { pathname } = useLocation();
+	const { pathname } = useLocation()
 
-    useEffect(
-        () => {
-            const element = document.getElementById(location.hash.replace("#", ""));console.log(element)
-            setTimeout(() => {
-                window.scrollTo({
-                    behavior: element ? "smooth" : "auto",
-                    top: element ? element.offsetTop : 0
-                });
-            }, 100);
-        }, [pathname]);
+	useEffect(() => {
+		const element = document.getElementById(location.hash.replace('#', ''))
+		setTimeout(() => {
+			window.scrollTo({
+				behavior: element ? 'smooth' : 'auto',
+				top: element ? element.offsetTop : 0,
+			})
+		}, 100)
+	}, [pathname])
 
-    return null;
-};  
+	return null
+}
 
-export default ScrollToTop;
+export default ScrollToTop
