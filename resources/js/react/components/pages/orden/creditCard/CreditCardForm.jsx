4d5fcfd326f5e919c1e2ad029088e3cf34d362d7
Epsilonxaos <angelotr12@hotmail.com>
2024-07-30 14:41:18 -0600

    refactor(Web): :fire: Eliminando archivos no usados

--- resources/js/react/components/pages/orden/creditCard/CreditCardForm.jsx
+++
@@ -1,6 +0,0 @@
-import React from 'react'
-import StripeForm from './StripeForm'
-
-export default function CreditCardForm({ pasoActualRoute = 'reservacion' }) {
-	return <StripeForm />
-}
--- resources/js/react/context/OrdenContext.jsx
+++
@@ -1,5 +0,0 @@
-import React, { createContext } from 'react';
-
-const OrdenContext = createContext();
-
-export default OrdenContext;
